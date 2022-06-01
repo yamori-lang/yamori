5002d72d5b3f0ef4505fc726be3640a50e1772fd
uchiryo7 <uchiryo7@gmail.com>
2022-06-01 12:25:23 +0900

    [WIP] Prog implement "llvm_lowering_pass.rs"

--- src/pass.rs
+++ src/pass.rs
@@ -1,39 +1,49 @@
 use crate::{block, diagnostic, function, int_kind, node, prototype, void_kind};
 
+pub struct PassContext {
+  diagnostics: Vec<diagnostic::Diagnostic>,
+}
+
+pub type PassResult = Result<(), diagnostic::Diagnostic>;
+
 pub trait Pass {
   fn register(&self) -> bool {
     return true;
   }
 
-  fn visit(&mut self, node: &dyn node::Node) {
+  fn visit(&mut self, node: &dyn node::Node) -> PassResult {
     // TODO:
     // node.accept(self);
-    self.visit_children(node);
+    self.visit_children(node)?;
+
+    Ok(())
   }
 
-  fn visit_children(&mut self, node: &dyn node::Node) {
+  fn visit_children(&mut self, node: &dyn node::Node) -> PassResult {
     for child in node.get_children() {
-      self.visit(child);
+      self.visit(child)?;
     }
+
+    Ok(())
   }
 
-  fn visit_block(&mut self, _: &block::Block) {
-    //
+  fn visit_block(&mut self, _: &block::Block) -> PassResult {
+    Ok(())
   }
 
-  fn visit_function(&mut self, _: &function::Function) {
-    //
+  fn visit_function(&mut self, _: &function::Function) -> PassResult {
+    Ok(())
   }
 
-  fn visit_prototype(&mut self, _: &prototype::Prototype) {
-    //
+  fn visit_prototype(&mut self, _: &prototype::Prototype) -> PassResult {
+    Ok(())
   }
 
-  fn visit_int_kind(&mut self, _: &int_kind::IntKind) {
-    //
+  fn visit_int_kind(&mut self, _: &int_kind::IntKind) -> PassResult {
+    Ok(())
   }
 
-  fn visit_void_kind(&mut self, _: &void_kind::VoidKind) {
-    //
+  fn visit_void_kind(&mut self, _: &void_kind::VoidKind) -> PassResult {
+    Ok(())
   }
 }
